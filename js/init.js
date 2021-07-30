5f647f1360ccd28ada10ffb31bc56428613da0c5
edmondywen <edmondywen@gmail.com>
2021-07-29 20:30:35 -0700

    setting up basic format of the map page

--- js/init.js
+++ js/init.js
@@ -3,3 +3,6 @@ const myMap = L.map('mapArea').setView([34.0709, -118.444], 5);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
+
+let slideshow = document.getElementById("slideimg")
+slideshow.src = "images/bliss.jpeg"
